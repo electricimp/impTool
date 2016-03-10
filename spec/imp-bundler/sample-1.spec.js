e025c4f20836478e88d63fc35b24c786691c2e19
Mikhail Yurasov <me@yurasov.me>
2016-03-10 15:44:27 -0800

    Add blank spec

---
+++ spec/imp-bundler/sample-1.spec.js
@@ -0,0 +1,17 @@
+/**
+ * Spec for ImpBundler
+ * @author Mikhail Yurasov <mikhail@electricimp.com>
+ */
+
+'use strict';
+
+describe('ImpBundler test suite', () => {
+
+  beforeEach(() => {
+  });
+
+  it('should process sample alright', (done) => {
+    done();
+  });
+
+});
