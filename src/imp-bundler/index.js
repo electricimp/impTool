7c96cb3387fec3fa19282a3ebe4f19da7c063dee
Mikhail Yurasov <me@yurasov.me>
2016-03-10 17:59:48 -0800

    Add ImpBundler errorrs

--- src/imp-bundler/index.js
+++ src/imp-bundler/index.js
@@ -5,8 +5,31 @@
 
 'use strict';
 
-class ImpBundler {
+const c = require('colors');
+const Errors = require('./Errors');
 
+class ImpBundler {
+  /**
+   * Debug print
+   * @param {*} ...objects
+   * @protected
+   */
+  _debug() {
+    if (this.debug) {
+      const args = Array.prototype.slice.call(arguments);
+      args.unshift(c.green('[debug:' + this.constructor.name + ']'));
+      console.log.apply(this, args);
+    }
+  };
+
+  get debug() {
+    return !!this._debug;
+  }
+
+  set debug(value) {
+    this._debug = !!value;
+  }
 }
 
 module.exports = ImpBundler;
+module.exports.Errors = Errors;
